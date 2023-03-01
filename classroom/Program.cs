a3327ff1b25aece2d18ee6d830918d56515c2fcf
Anna <shiskanovaag@mail.ru>
2023-03-01 15:35:48 +1000

    Add task42

--- classroom/Program.cs
+++ classroom/Program.cs
@@ -1,9 +1,53 @@
-﻿Task39();
+﻿Console.Clear();
+Console.WriteLine("1 - Task 39");
+Console.WriteLine("2 - Task 40");
+Console.WriteLine("3 - Task 42");
+Console.WriteLine("4 - Task 35");
+Console.WriteLine("5 - Task 37");
+int NumberOfTask = Prompt("Введите номер задачи");
+switch(NumberOfTask)
+{
+    case 1:
+    Console.Clear();
+    Task39();
+    break;
 
+    case 2:
+    Console.Clear();
+    //Task40();
+    break;
 
+    case 3:
+    Console.Clear();
+    Task42();
+    break;
+
+    // case 4:
+    // Console.Clear();
+    // Task35();
+    // break;
+
+    // case 5:
+    // Console.Clear();
+    // Task37();
+    // break;
+
+    default:
+    Console.WriteLine("Номер задачи введен неверно");
+    break;
+    
+}
+
+static int Prompt(string massage)//метод запроса числа
+{
+    Console.WriteLine(massage);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
 
 
 static void Task39()
+
 {//Задача 39: Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.)
 //[1 2 3 4 5] -> [5 4 3 2 1]
 //[6 7 3 6] -> [6 3 7 6]
@@ -48,4 +92,44 @@ return tmpArray;
     PrintArray(array);
     PrintArray(ReverseArray(array));
     PrintArray(AnotherReverseArray(array));
+}
+
+// static void Task40()
+// {
+//     //     Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, 
+//     // может ли существовать треугольник с сторонами такой длины.
+//     // Теорема о неравенстве треугольника: 
+//     // каждая сторона треугольника меньше суммы двух других сторон.
+
+//     int a;
+//     int b;
+//     int c;
+//     int Sum1 = a + b;
+//     int Sum2 = b + c;
+//     int Sum3 = a + c;
+//     if (Sum1<Sum2 && Sum1<Sum3 && Sum2<Sum3)
+//     true;
+
+
+
+// }
+
+static void Task42()
+{
+    // Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+    Console.WriteLine("Input number: ");
+    int numb = Convert.ToInt32(Console.ReadLine());
+    string numbs = string.Empty;
+
+    while (numb > 0)
+    {
+        numbs = numb % 2 + numbs;
+        numb = numb / 2;
+    }
+
+    System.Console.WriteLine(numbs);
+
 }
